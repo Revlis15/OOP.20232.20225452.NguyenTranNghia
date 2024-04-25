5631a3b53c8ea0457ad4db36c0345320703473ad
Revlis15 <bopbin15@gmail.com>
2024-04-25 10:11:31 +0700

    final

---
+++ Lab01/src/hust/soict/dsai/lab01/EX66.java
@@ -0,0 +1,56 @@
+package hust.soict.dsai.lab01;
+import java.util.Scanner;
+public class EX66 {
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows and columns for the matrices: ");
+        int rows = sr.nextInt();
+        int columns = sr.nextInt();
+
+        int[][] matrix1 = new int[rows][columns];
+        int[][] matrix2 = new int[rows][columns];
+
+        System.out.println("Enter the elements of the first matrix:");
+        inputMatrix(sr, matrix1);
+
+        System.out.println("Enter the elements of the second matrix:");
+        inputMatrix(sr, matrix2);
+
+        int[][] sumMatrix = addMatrices(matrix1, matrix2);
+
+        System.out.println("The sum of the matrices is:");
+        printMatrix(sumMatrix);
+    }
+
+    public static void inputMatrix(Scanner sr, int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = sr.nextInt();
+            }
+        }
+    }
+
+    public static int[][] addMatrices(int[][] matrix1, int[][] matrix2) {
+        int rows = matrix1.length;
+        int columns = matrix1[0].length;
+        int[][] sumMatrix = new int[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+
+        return sumMatrix;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Lab02/AimsProject/hust/soict/dsai/disc/DigitalVideoDisc.java
@@ -0,0 +1,116 @@
+package hust.soict.dsai.disc;
+
+public class DigitalVideoDisc {
+	public String title;
+	public String category;
+	public String directory;
+	public int length;
+	public float cost;
+	private static int nbDigitalVideoDiscs = 0;
+	private int id;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDirectory() {
+		return directory;
+	}
+	public void setDirectory(String directory) {
+		this.directory = directory;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public float getCost() {
+		return cost;
+	}
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+	}
+	public DigitalVideoDisc(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
+	public DigitalVideoDisc(String title, String category, String directory, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.directory = directory;
+		this.cost = cost;
+	}
+	public DigitalVideoDisc(String title, String category, String directory, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.directory = directory;
+		this.length = length;
+		this.cost = cost;
+	}
+	public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
+    }
+	public int getId() {
+        return id;
+    }
+	public DigitalVideoDisc(String title, String category, String directory, int length, float cost,int id) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.directory = directory;
+		this.length = length;
+		this.cost = cost;
+		
+		nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+	}
+	@Override
+	public String toString() {
+		return "DVD - " + title + " - " + category + " - " + directory + " - "
+				+ length + ": " + cost + "$";
+	}
+	public boolean isMatch(String title) {
+		String[] words = title.split(" ");
+		for (String word : words) {
+			if (!this.title.contains(word)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	public boolean isMatch(int id) {
+		return this.id == id;
+	}
+	
+	public void search(String title) {
+		if (isMatch( 	title)) {
+			System.out.println("Found a match: " + this.title);
+		} else {
+			System.out.println("No match found! Try again.");
+		}
+	}
+	
+	public void search(int id) {
+		if (isMatch(id)) {
+			System.out.println("Found a match: " + this.title);
+		} else {
+			System.out.println("No match found! Try again.");
+		}
+	}
+}
